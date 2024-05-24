df119ce6fcda1d63228251f751a6f8d55bf4c6b6
lexandfr <alexfloresrayme@gmail.com>
2024-05-24 10:06:53 -0500

    chore: Add new hooks for opening and confirming actions

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
 import { useMountedState } from "react-use";
+
+import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
+import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
+
+import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
+import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
 
 export const SheetProvider = () => {
   const isMounted = useMountedState();
@@ -13,6 +18,10 @@ export const SheetProvider = () => {
   return (
     <>
       <NewAccountSheet />
+      <EditAccountSheet />
+
+      <NewCategorySheet />
+      <EditCategorySheet />
     </>
   );
 };

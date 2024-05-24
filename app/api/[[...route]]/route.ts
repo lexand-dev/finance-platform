336ff529dd91dcb6791e27f1a4717488c8ca3e33
lexandfr <alexfloresrayme@gmail.com>
2024-05-24 10:10:21 -0500

    feat: Add CRUD for managing categories

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,23 +2,19 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 import accounts from "./accounts";
+import categories from "./categories";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-/* app.onError((err, c) => {
-  if (err instanceof HTTPException) {
-    return err.getResponse();
-  }
-
-  return c.json({ error: "Internal Server Error" }, 500);
-}) */
-
 const routes = app
   .route("/accounts", accounts)
+  .route("/categories", categories);
 
 export const GET = handle(app);
 export const POST = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
 
 export type AppType = typeof routes;

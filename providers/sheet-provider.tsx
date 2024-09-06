8d7c3648bd4d0b80ad538d66469ff4f903052bcf
lexandfr <alexfloresrayme@gmail.com>
2024-09-06 14:05:47 -0500

    fix import provider

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -8,8 +8,8 @@ import { NewAccountSheet } from "@/features/accounts/components/new-account-shee
 import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
 import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
 
-import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
-
+/* import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
+ */
 export const SheetProvider = () => {
   const isMounted = useMountedState();
 
@@ -25,7 +25,7 @@ export const SheetProvider = () => {
       <NewCategorySheet />
       <EditCategorySheet />
 
-      <NewTransactionSheet />
+      {/*   <NewTransactionSheet /> */}
       {/* <EditTransactionSheet /> */}
     </>
   );

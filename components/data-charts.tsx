7669b43f13929c4804fd26d30ec35d09ecb508c2
alexander flores <alexfloresrayme@gmail.com>
2024-10-22 21:46:13 -0500

    create spending pie for categories data with chart variants

--- components/data-charts.tsx
+++ components/data-charts.tsx
@@ -3,6 +3,7 @@
 import { useGetSummary } from "@/features/summary/api/use-get-summary";
 
 import { Chart, ChartLoading } from "@/components/chart";
+import { SpendingPie, SpendingPieLoading } from "@/components/spending-pie";
 
 export const DataCharts = () => {
   const { data, isLoading } = useGetSummary();
@@ -13,6 +14,9 @@ export const DataCharts = () => {
         <div className="col-span-1 lg:col-span-3 xl:col-span-4">
           <ChartLoading />
         </div>
+        <div className="col-span-1 lg:col-span-3 xl:col-span-2">
+          <SpendingPieLoading />
+        </div>
       </div>
     );
   }
@@ -21,6 +25,9 @@ export const DataCharts = () => {
     <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
       <div className="col-span-1 lg:col-span-3 xl:col-span-4">
         <Chart data={data?.days} />
+      </div>
+      <div className="col-span-1 lg:col-span-3 xl:col-span-2">
+        <SpendingPie data={data?.categories} />
       </div>
     </div>
   );
